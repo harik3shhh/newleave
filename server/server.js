06b62f98f2f2abc22f12e580cfe4471f678759f0
harikesh <138229790+harik3shhh@users.noreply.github.com>
2024-05-14 09:27:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const port = process.env.PORT;
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: "GET, POST, PUT, DELETE, HEAD",
     credentials: true,
 };
